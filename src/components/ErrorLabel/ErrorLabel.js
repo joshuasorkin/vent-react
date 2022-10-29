1ecdce26fac8cc2a7f25874985ab1f711155fca7
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-10-28 23:30:47 -0700

    Revert "initial commit"
    This reverts commit 640045542778bab1d2260fb0047bf7fbcd9bf008.

--- src/components/ErrorLabel/ErrorLabel.js
+++
@@ -1,9 +0,0 @@
-function ErrorLabel(props){
-    return(
-        <div className="error">
-            <label htmlFor="error" className="form__label">{props.errorText}</label>
-        </div>
-    );
-}
-
-export default ErrorLabel;
--- src/components/RegistrationForm/InputItem/InputItem.js
+++
@@ -1,8 +0,0 @@
-export default function InputItem(props){
-    return(
-        <div className={props.inputName}>
-            <label htmlFor={props.inputName} className="form__label">{props.labelText}</label>
-            <input value={props.value} onChange={props.handleInputChange} type={props.type} id={props.inputName} placeholder={props.labelText} className="form__input" />
-        </div>
-    )
-}
